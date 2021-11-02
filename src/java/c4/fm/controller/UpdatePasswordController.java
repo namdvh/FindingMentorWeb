aee3770cd5e280e616439ca8cfdd2462901c85a7
nguyenhuutoan05697 <nguyenhuutoan05697@gmail.com>
2021-11-02 11:49:41 +0700

    Fix bug

--- src/java/c4/fm/controller/UpdatePasswordController.java
+++ src/java/c4/fm/controller/UpdatePasswordController.java
@@ -17,7 +17,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import static sun.java2d.cmm.ColorTransform.In;
+
 
 /**
  *
